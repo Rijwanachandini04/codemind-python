e144ae5c7a394773d51f09044dd5ecf1bd313cb8
Rijwanachandini04 <97875833+Rijwanachandini04@users.noreply.github.com>
2022-10-14 20:32:40 +0530

    l=list(map(str,input().split())) a=[] for i in l:     a.append(min(i))     a.append(max(i)) print(*a)

--- small_and_large_characters.py
+++ small_and_large_characters.py
@@ -1,10 +1,6 @@
-a=input()
-arr=list(a.split())
-for i in arr:
-    mi=ma=i[0]
-    for j in i:
-        if ord(j)>ord(ma):
-            ma=j
-        if ord(j)<ord(mi):
-            mi=j
-    print(mi,ma,end=" ")
+l=list(map(str,input().split()))
+a=[]
+for i in l:
+    a.append(min(i))
+    a.append(max(i))
+print(*a)
