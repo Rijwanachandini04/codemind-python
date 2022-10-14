f94a84d99fe450a2d315a8ae48f0fa2f9c058c1e
Rijwanachandini04 <97875833+Rijwanachandini04@users.noreply.github.com>
2022-10-14 20:27:53 +0530

    n=input() n=n.lower() if n==n[::-1]:     print("True") else:     print("False")

--- Palindrome_or_not.py
+++ Palindrome_or_not.py
@@ -1,13 +1,6 @@
-a=input()
-f=0
-le=len(a)
-for i in range(le):
-    if (a[i]==a[le-i-1] or a[i]==a[le-i-1].upper() or a[i]==a[le-i-1].lower()):
-        f=1
-    else:
-        f=0
-        break
-if f==1:
+n=input()
+n=n.lower()
+if n==n[::-1]:
     print("True")
 else:
     print("False")
