35a3fbe9f6e617fe7051ef158114a593bc72923d
Rijwanachandini04 <97875833+Rijwanachandini04@users.noreply.github.com>
2022-10-14 20:29:27 +0530

    s=input() ss=set(s) if len(s)==len(ss):     print(True) else:     print(False)

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,21 +1,6 @@
-a=input()
-c=wlc=0
-l=len(a)
-for i in range(len(a)):
-    c=0
-    for j in range(len(a)):
-        if i!=j:
-            if a[i]==a[j]:
-                c+=1
-    if c==0:
-        wlc+=1
-    else:
-        wlc=0
-        break
-if wlc==0:
-    print("False")
+s=input()
+ss=set(s)
+if len(s)==len(ss):
+    print(True)
 else:
-    if wlc==l:
-        print("True")
-    else:
-        print("False")
+    print(False)
