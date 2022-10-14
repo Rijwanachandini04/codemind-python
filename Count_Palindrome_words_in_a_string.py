84e68b1855c02863318588a1372d1ebfad5018cd
Rijwanachandini04 <97875833+Rijwanachandini04@users.noreply.github.com>
2022-10-14 20:23:27 +0530

    l=list(map(str,input().split())) c=0 for i in l:     i=i.lower()     if i==i[::-1]:         c+=1 print(c)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,23 +1,7 @@
-def pal(s):
-    s1=""
-    for i in s:
-        if i.isupper():
-            s1+=i.lower()
-        else:
-            s1+=i
-    if s1==rev(s1):
-        return 1
-    else:
-        return 0
-def rev(s):
-    s1=""
-    for i in range(len(s)-1,-1,-1):
-        s1+=s[i]
-    return s1
-a=input()
-arr=list(a.split())
+l=list(map(str,input().split()))
 c=0
-for i in arr:
-    if pal(i)==1:
+for i in l:
+    i=i.lower()
+    if i==i[::-1]:
         c+=1
 print(c)
