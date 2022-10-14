13537a289c1f4e580de30bf2d7b4d533cc931217
Rijwanachandini04 <97875833+Rijwanachandini04@users.noreply.github.com>
2022-10-14 20:18:52 +0530

    l=list(map(str,input().split())) a=[] a.append(min(l[0])) a.append(max(l[len(l)-1])) print(*a)

--- small_and_large_elements.py
+++ small_and_large_elements.py
@@ -1,12 +1,5 @@
-a=input()
-arr=list(a.split())
-l=len(arr)
-mi=arr[0][0]
-ma=arr[l-1][0]
-for i in arr[0]:
-    if ord(i)<ord(mi):
-        mi=i
-for i in arr[l-1]:
-    if ord(i)>ord(ma):
-        ma=i
-print(mi,ma)
+l=list(map(str,input().split()))
+a=[]
+a.append(min(l[0]))
+a.append(max(l[len(l)-1]))
+print(*a)
