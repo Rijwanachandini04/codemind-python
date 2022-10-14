f0c00341d0853c4228ea161642a2d88457dd2ac7
Rijwanachandini04 <97875833+Rijwanachandini04@users.noreply.github.com>
2022-10-14 20:14:17 +0530

    l=list(map(str,input().split())) ll=[] ll1=[] s=[] for i in l:     ll.append(i.lower()) l1=list(map(str,input().split())) for i in l1:     ll1.append(i.lower()) for i in ll:     if i in ll1:         s.append(i) print(len(s))

--- common_words_-1.py
+++ common_words_-1.py
@@ -1,31 +1,13 @@
-def word(s):
-    s1=""
-    for i in s:
-        s1+=i.lower()
-    return s1
-a=input()
-b=input()
-c=list(a.split())
-d=list(b.split())
-w1=[]
-for i in c:
-    if word(i) not in w1:
-        w1.append(word(i))
-w2=[]
-for i in d:
-    if word(i) not in w2:
-        w2.append(word(i))
-co=0
-if len(w1)>len(w2):
-    for i in w2:
-        if i in w1:
-            co+=1
-elif len(w1)<len(w2):
-    for i in w1:
-        if i in w2:
-            co+=1
-else:
-    for i in w1:
-        if i in w2:
-            co+=1
-print(co)
+l=list(map(str,input().split()))
+ll=[]
+ll1=[]
+s=[]
+for i in l:
+    ll.append(i.lower())
+l1=list(map(str,input().split()))
+for i in l1:
+    ll1.append(i.lower())
+for i in ll:
+    if i in ll1:
+        s.append(i)
+print(len(s))
